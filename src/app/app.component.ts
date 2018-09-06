f1de67eefe904aed7e55e78992147ce27d8b140b
kondareddyyaramala <kondareddy.yaramala@gmail.com>
2018-09-05 20:19:15 -0500

    From and Filter operator examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { of, Observable, from, concat } from 'rxjs';
+import { filter, catchError, tap, concatAll } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ap';
+  public array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  public formattedArray = [];
+
+  ngOnInit() {
+    this.fromAndFilterExample();
+  }
+
+  // Creating an array from the static array and filtering to get out even numbers 
+  // and then subscribing to it and concatenating to the array
+  fromAndFilterExample() {
+    const obs = from(this.array).pipe(
+      filter(v => v % 2 === 0),
+    )
+    obs.subscribe(v => this.formattedArray.push(v));
+  }
 }
